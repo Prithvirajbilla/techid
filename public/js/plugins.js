38694162de19b6021077dd040d4eaad0a6f58049
PrithvirajBilla <prithviraj.m.billa@gmail.com>
2013-07-16 01:58:23 +0530

    Adding existing files and plugins

---
+++ public/js/plugins.js
@@ -0,0 +1,276 @@
+$(document).ready(function(){
+    // Bootstrap tooltip
+    $(".tip").tooltip({placement: 'top', trigger: 'hover'});
+    $(".tipb").tooltip({placement: 'bottom', trigger: 'hover'});
+    $(".tipl").tooltip({placement: 'left', trigger: 'hover'});
+    $(".tipr").tooltip({placement: 'right', trigger: 'hover'});
+                 
+    if($(".fb").length > 0)
+        $("a.fb").fancybox({padding: 10,
+                                'transitionIn'  : 'elastic',
+                                'transitionOut' : 'elastic',
+                                'speedIn'       : 600, 
+                                'speedOut'      : 200
+                            });
+    // Uniform
+        $("input:checkbox, input:radio").not('input.ibtn').uniform();    
+    // Select2
+    if($(".select").length > 0){
+        $(".select").select2();
+        $(".select").on("change", function(e) {             
+            //action
+        });
+    }
+        
+    
+    
+    // Validation
+    if($("#validate").length > 0)
+        $("#validate, #validate_custom").validationEngine('attach',{promptPosition : "topLeft"});
+    
+    // Datepicker
+    $(".datepicker").datepicker({dateFormat: 'yy-mm-dd'});
+    
+    if($("#datepicker").length > 0){
+        
+        $( "#datepicker" ).datepicker({dateFormat: 'yy-mm-dd',
+                                       onSelect: function(date){
+                                            //action
+                                     }});
+    }
+        
+    
+    // Wizard
+    if($("#wizard").length > 0) $('#wizard').stepy();
+    
+    if($("#wizard_validate").length > 0){
+        
+        $("#wizard_validate").validationEngine('attach',{promptPosition : "topLeft"});
+        
+        $('#wizard_validate').stepy({
+            back: function(index) {                                                                
+                //if(!$("#wizard_validate").validationEngine('validate')) return false; //uncomment if u need to validate on back click                
+            }, 
+            next: function(index) {                
+                if(!$("#wizard_validate").validationEngine('validate')) return false;                
+            }, 
+            finish: function(index) {                
+                if(!$("#wizard_validate").validationEngine('validate')) return false;
+            }            
+        });
+    }
+
+    if($("#wizard_ajax").length > 0){
+        
+        $("#wizard_ajax").validationEngine('attach',{promptPosition : "topLeft"});
+        
+        $('#wizard_ajax').stepy({
+            back: function(index) {                
+                return false;
+            }, 
+            next: function(index) {                
+                if(!$("#wizard_ajax").validationEngine('validate')) return false;
+                
+                
+                
+                if((index-2) == 0){
+                    $.post('php/ajax_wizard.php',{login: $("#wizard_ajax input[name=login]").val(),
+                                                email: $("#wizard_ajax input[name=email]").val()},
+                                                function(data){
+                                                        if(data == 'success')
+                                                            $('#wizard_ajax').stepy('step', index);
+                                                        else
+                                                            $('#wizard_ajax input[name=login]').validationEngine('showPrompt', 'Response doesn\'t match', 'error','topLeft', true);
+                                                });                    
+                    return false;
+                }                                                
+                                
+                if((index-2) == 1){
+                    $.post('php/ajax_wizard.php',{hash: $("#wizard_ajax input[name=hash]").val()},
+                                                  function(data){
+                                                    if(data == 'success')
+                                                        $('#wizard_ajax').stepy('step', index);
+                                                    else
+                                                        $('#wizard_ajax input[name=hash]').validationEngine('showPrompt', 'Response doesn\'t match', 'error','topLeft', true);
+                                                  });                                
+                    return false;
+                }
+
+            },
+            finish: function(index) {                
+                if(!$("#wizard_ajax").validationEngine('validate')) return false;
+                
+                //action
+            }            
+        });
+    }
+
+    // eof wizard
+    
+    // accordion
+    if($(".accordion").length > 0) $(".accordion").accordion({heightStyle: "content"});
+    // eof accordion
+    
+    // tabs
+    if($(".tabs").length > 0) $(".tabs").tabs();
+    // eof tabs
+    
+    // sortable    
+    if($("#sort_1").length > 0){
+        $("#sort_1").sortable();
+        $("#sort_1").disableSelection();    
+    }
+    // eof sortable
+    
+    // selectable 
+    if($("#select_1").length > 0) $("#select_1").selectable();
+    //eof selectable
+    
+    // progressbars
+    if($("#progressbar_default").length > 0)
+        $("#progressbar_default").progressbar({value: 65});
+    
+    if($("#progressbar_animated").length > 0){        
+        $("#progressbar_animated").progressbar({value: 0});
+        $("#sAProgress").click(function(){
+            $("#progressbar_animated").progressbar('destroy');
+            var iNow = new Date().setTime(new Date().getTime() + 0 * 1000);
+            var iEnd = new Date().setTime(new Date().getTime() + 5 * 1000);
+            $("#progressbar_animated").anim_progressbar({start: iNow, finish: iEnd, interval: 1});
+        });
+    }
+    
+    if($("#progressbar_delay").length > 0){        
+        $("#progressbar_delay").progressbar({value: 0});
+        $("#sWDProgress").click(function(){
+            $("#progressbar_delay").progressbar('destroy');
+            var iNow1 = new Date().setTime(new Date().getTime() + 3 * 1000);
+            var iEnd1 = new Date().setTime(new Date().getTime() + 6 * 1000);
+            $("#progressbar_delay").anim_progressbar({start: iNow1, finish: iEnd1, interval: 1});
+        });
+    }
+    // eof progressbars
+    
+    // spinner
+        $( "#spinner" ).spinner();
+        $( "#spinner1" ).spinner({culture: "en-US", min: 5, max: 1000, step: 10, start: 10, numberFormat: "C"});
+    // eof spinner
+    
+    // sliders
+        $("#slider_1").slider({
+            value: 60,
+            orientation: "horizontal",
+            range: "min",
+            animate: true,
+            slide: function( event, ui ) {
+                $( "#slider_1_amount" ).html( "$" + ui.value );
+            },
+            stop: function( event, ui ) {
+                //action
+            }
+        });
+        
+        $("#slider_2").slider({
+            values: [ 17, 67 ],
+            orientation: "horizontal",
+            range: true,
+            animate: true,
+            slide: function( event, ui ) {
+                $( "#slider_2_amount" ).html( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+            },
+            stop: function( event, ui ) {
+                //action
+            }            
+        });    
+            
+        $("#slider_3").slider({
+            orientation: "vertical",
+            range: "min",
+            min: 0,
+            max: 100,
+            value: 10,            
+            stop: function( event, ui ) {
+                //action
+            }            
+        }); 
+
+        $("#slider_4").slider({
+            orientation: "vertical",
+            range: true,
+            values: [ 17, 67 ],
+            stop: function( event, ui ) {
+                //action
+            }
+        }); 
+
+        $("#slider_5").slider({
+            orientation: "vertical",            
+            range: "max",
+            min: 1,
+            max: 10,
+            value: 2,
+            stop: function( event, ui ) {
+                //action
+            }            
+        });     
+    // eof sliders
+    
+    // popovers
+    
+    $("#popover_top").popover({placement: 'top', title: 'Popover title', content: 'Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor velit.'});
+    $("#popover_right").popover({placement: 'right', title: 'Popover title', content: 'Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor velit.'});
+    $("#popover_bottom").popover({placement: 'bottom', title: 'Popover title', content: 'Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor velit.'});
+    $("#popover_left").popover({placement: 'left', title: 'Popover title', content: 'Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor velit.'});
+    
+    // eof popovers
+    
+    // jQuery dialogs
+
+        
+        
+        $("#jDialog_default_button").click(function(){
+            $("#jDialog_default").dialog({autoOpen: false}).dialog('open');
+        });
+        
+        $("#jDialog_modal_button").click(function(){
+            $("#jDialog_modal").dialog({autoOpen: false, modal: true}).dialog('open');
+        });        
+    
+        $("#jDialog_form_button").click(function(){
+            $("#jDialog_form").dialog({autoOpen: false, 
+                                    modal: true,
+                                    width: 400,
+                                    buttons: {                            
+                                        "Submit": function() {
+                                            $( this ).dialog( "close" );
+                                        },
+                                        Cancel: function() {
+                                            $( this ).dialog( "close" );
+                                        }
+            }}).dialog('open');                        
+        });        
+        
+        
+        
+    // eof dialogs
+        
+
+});
+
+$(window).load(function(){
+
+    // custom scrollbar        
+    if($(".scroll").length > 0)
+        $(".scroll").mCustomScrollbar();
+    // eof custom scrollbar    
+    
+});
+
+$('.wrapper').resize(function(){
+
+    if($("#wysiwyg").length > 0) editor.refresh();
+    if($("#mail_wysiwyg").length > 0) m_editor.refresh();
+    
+});
+
+
