939aa2f405ed5a4434d19df73aa1899bab74a2ec
Hetal <hetalrathod405@gmail.com>
2013-08-17 01:05:11 +0530

    added admin panel

---
+++ admin/config.php
@@ -0,0 +1,9 @@
+<?php
+	// Connection's Parameters
+	$db_host="localhost";
+	$db_name="techid";
+	$username="root";
+	$password="root";
+	mysql_connect($db_host,$username,$password);
+	mysql_select_db($db_name);
+?>
---
+++ admin/index.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+
+	<link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.min.css">
+
+</head>
+<body>
+    <h1>Hello, world!</h1>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Enable responsive features in IE8 with Respond.js (https://github.com/scottjehl/Respond) -->
+    <script src="js/respond.js"></script>
+ <form class="form-horizontal">
+<fieldset>
+
+<!-- Form Name -->
+<legend>techid_login</legend>
+
+<!-- Text input-->
+<div class="control-group">
+  <label class="control-label" for="Techid">Tech ID</label>
+  <div class="controls">
+    <input id="Techid" name="Techid" type="text" placeholder="ldap id" class="input-xlarge" required="">
+    
+  </div>
+</div>
+
+<!-- Password input-->
+<div class="control-group">
+  <label class="control-label" for="password">Password </label>
+  <div class="controls">
+    <input id="password" name="password" type="password" placeholder="" class="input-xlarge" required="">
+    
+  </div>
+</div>
+
+<!-- Button -->
+<div class="control-group">
+  <label class="control-label" for="submitbutton"></label>
+  <div class="controls">
+    <button id="submitbutton" name="submitbutton" class="btn btn-default">Submit</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
+
+  </body>
+  </html>
