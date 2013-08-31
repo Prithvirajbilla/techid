eb686cff98aa389c7a37134a240ecadda3e7dd06
PrithvirajBilla <prithviraj.m.billa@gmail.com>
2013-08-31 18:16:58 +0530

    cleaning

--- awesome.php
+++
@@ -1,8 +0,0 @@
-<?php include_once("include/head.php"); ?>
-<body >
-    <div class="wrapper">
-        <div class="top">
-            <a href="/techid" class="logo"></a>
-        </div>
-        <div class="body">
-<?php include_once("include/nav.php"); ?>
